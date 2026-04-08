e8ac0e9a74d22c6146f5607879ba83aef7b45479
Unity-Square-Labs <218101832+Unity-Square-Labs@users.noreply.github.com>
2026-04-08 08:35:48 -0400

    Update active.bat

--- batch/active.bat
+++ batch/active.bat
@@ -1,20 +1,6 @@
 @echo off
 :loop
-for /f "tokens=1-2 delims=:" %%a in ("%time%") do (
-    set hour=%%a
-    set min=%%b
-)
-set hour=%hour: =%
-if %hour% LSS 8 goto exit
-if %hour% GTR 17 goto exit
-if %hour% EQU 17 if %min% GTR 30 goto exit
-
 powershell -command "$wsh = New-Object -ComObject WScript.Shell; $wsh.SendKeys('{SCROLLLOCK}')"
 echo Running at %time%
-
-timeout /t 60 /nobreak >nul
+timeout /t 300 /nobreak >nul
 goto loop
-
-:exit
-echo Outside work hours (%time%). Exiting.
-exit
